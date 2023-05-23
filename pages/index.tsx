746568d36e7727b763d0048a5734a6f35b1ec406
so9to <47969821+9-to@users.noreply.github.com>
2023-05-23 20:59:20 +0900

    change a bit

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,15 @@ const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
         setLoading(false);
     };
     return (
-        <div className={styles.page}>
-            <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+        <div>
+            <button onClick={handleClick}
+            style={{
+                backgroundColor:"#319795",
+                border: "none",
+                borderRadius: "4px",
+                color: "white",
+                padding: "4px 8px",
+            }}>昨日のにゃんこ😺</button>
             <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img}/>}</div>
         </div>
     );
